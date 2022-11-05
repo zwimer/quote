6cf1c4755f75d9640f5c9fa2053adbbcfb00c446
zwimer <zwimer@gmail.com>
2022-11-04 17:54:54 -0700

    first

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # quote
+
 A tiny CLI used for quoting input lines
+
+## Example
+
+Consider the directory:
+```bash
+$ find .
+.
+./c
+./a b
+```
+
+Here, `xargs` alone will fail if we do: `find . -type f | xargs cat` with:
+```
+This file is named: c
+cat: ./a: No such file or directory
+cat: b: No such file or directory
+```
+
+This is because the file 'a b' is not quoted!
+
+Instead we can do: `find . -type f | quote | xargs cat` and we will get:
+```
+This file is named: c
+This file is named: a b
+```
---
+++ quote/quote.py
@@ -0,0 +1,38 @@
+from typing import List
+import argparse
+import shlex
+import sys
+import os
+
+from . import __version__
+
+
+def quote(skip_empty: bool, newline: bool, keep_trailing_newline: bool) -> None:
+    # Read in stdin
+    data: str = "".join(line for line in sys.stdin)
+    if not keep_trailing_newline and data.endswith("\n"):
+        data = data[:-1]
+    # Quote lines
+    items: List[str] = data.split("\n")
+    if skip_empty:
+        items = [ i for i in items if len(i) > 0 ]
+    quoted: List[str] = [ shlex.quote(i) for i in items ]
+    # Output
+    output: str = ("\n" if newline else " ").join(quoted)
+    print(output, flush=True)
+
+
+def parse_args(program: str, *args: str) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(prog=os.path.basename(program))
+    parser.add_argument("--version", action="version", version=f"{program} {__version__}")
+    parser.add_argument("-n", "--newline", action="store_true", default=False,
+        help="Output arguments with newline instead of a space as a delimiter")
+    parser.add_argument("--skip-empty", action="store_true", default=False,
+        help="Do not output empty quoted strings")
+    parser.add_argument("--keep_trailing_newline", action="store_true", default=False,
+        help="Do not ignore the final character if it is a newline")
+    return parser.parse_args(args)
+
+
+def cli():
+    quote(**vars(parse_args(*sys.argv)))
